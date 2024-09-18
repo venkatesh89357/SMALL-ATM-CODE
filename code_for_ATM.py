83082da4ed1798b842308b24ab2340ce04da698c
Venkatesh Gorla <140168875+venkatesh89357@users.noreply.github.com>
2024-09-18 19:45:29 +0530

    Update code_for_ATM.py

--- code_for_ATM.py
+++ code_for_ATM.py
@@ -1,6 +1,6 @@
 # Generating an ATM code for simple amount transfer and to check balance
 
-print(" Welcome To BANK OF BARODA")
+print(" Welcome To BANK OF META MASK")
 i=0
 #taking aone username and password to login the app
 username = "venkatesh"
